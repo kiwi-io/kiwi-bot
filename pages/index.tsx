a9dab992564a5a305d5b0e62f0ff4673516be356
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-09-18 21:10:13 +0530

    Set basic changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,42 @@
-import React from "react";  
+import React from "react";
+import { useEffect, useState } from "react";
   
 export default function Home() {  
+
+  const [telegramUser, setTelegramUser] = useState(null);
+
+  useEffect(() => {
+    //@ts-ignore
+    if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+      //@ts-ignore
+      const webApp = window.Telegram.WebApp;
+
+      // Initialize the Telegram Web App when the component loads
+      webApp.ready();
+
+      // Get and set user information
+      setTelegramUser(webApp.initDataUnsafe.user);
+
+      // Customize Web App appearance
+      webApp.MainButton.setText('Start');
+      webApp.MainButton.show();
+    }
+  }, []);
+
+
   return (
     <div>
         <h1>Hello world</h1>
+        {
+          telegramUser ? (
+            <p>Welcome, {
+              //@ts-ignore
+              telegramUser.first_nam
+            }!</p>
+          ) : (
+            <p>Loading Telegram User...</p>
+          )
+        }
     </div>
   );
 }
