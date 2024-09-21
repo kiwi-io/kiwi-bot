440bb1201196efa4a8127dfd14d8420785ac5af3
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-09-21 20:15:19 +0530

    Update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [`raw.githubusercontent.com`],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   env: {
     NEXT_PRIVY_APP_ID: process.env.NEXT_PRIVY_APP_ID || "",
     NEXT_PRIVY_SECRET: process.env.NEXT_PRIVY_SECRET || "",
