30b2be919a1325631e168992653aabb680795f25
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-09-22 20:04:15 +0530

    Add print env vars

--- utils/birdeye/index.ts
+++ utils/birdeye/index.ts
@@ -15,6 +15,8 @@ export interface TokenListItem {
 
 export const getTokenList = async() => {
     try {
+        console.log("BIRDEYE_GET_TOKEN_LIST: ", process.env.NEXT_BIRDEYE_GET_TOKEN_LIST);
+        console.log("API key: ", process.env.NEXT_BIRDEYE_API_KEY);
         const response = await axios.get(`${process.env.NEXT_BIRDEYE_GET_TOKEN_LIST}`, {
             headers: {
                 'Content-Type': 'application/json',
@@ -26,6 +28,6 @@ export const getTokenList = async() => {
 
         return response.data.tokens as TokenListItem[];
     } catch (error) {
-        console.error('Error fetching price:', error);
+        console.error('Error fetching token list:', error);
     }    
 }
