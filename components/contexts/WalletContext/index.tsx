41045b25e945be4d63788d0e35b94c31e422708c
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-09-24 12:22:21 +0530

    try 1

--- components/contexts/WalletContext/index.tsx
+++ components/contexts/WalletContext/index.tsx
@@ -65,8 +65,8 @@ export const WalletContextProvider = ({ children }) => {
     
     const updatePortfolio = async(user: User) => {
         if(user && user.wallet) {
-            const latestPortfolio = await getWalletPortfolio(user.wallet.address);
-            // let latestPortfolio = await getWalletPortfolio("4RetBVitL3h4V1YrGCJMhGbMNHRkhgnDCLuRjj8a9i1P");
+            // const latestPortfolio = await getWalletPortfolio(user.wallet.address);
+            let latestPortfolio = await getWalletPortfolio("4RetBVitL3h4V1YrGCJMhGbMNHRkhgnDCLuRjj8a9i1P");
             console.log("Latest portfolio: ", latestPortfolio, " on: ", Date.now());
             setPortfolio((_) => latestPortfolio);
         }
