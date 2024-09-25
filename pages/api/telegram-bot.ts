803eac937ba78ed78780d7516c4f408c28d11d26
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-09-25 16:54:07 +0530

    updated bot route

--- pages/api/telegram-bot.ts
+++ pages/api/telegram-bot.ts
@@ -1,9 +1,30 @@
 import bot from "../../server/telegram-bot";
 
+// export default async function handler(req: any, res: any) {
+//   if (req.method === 'POST') {
+//     await bot.handleUpdate(req.body); // Handle incoming Telegram updates
+//     res.status(200).send('OK');
+//   } else {
+//     res.status(405).end(); // Method Not Allowed
+//   }
+// }
+
 export default async function handler(req: any, res: any) {
   if (req.method === 'POST') {
-    await bot.handleUpdate(req.body); // Handle incoming Telegram updates
-    res.status(200).send('OK');
+    try {
+      // Initialize the bot
+      if (!bot.isInited()) {
+        await bot.init(); // Fetch bot info from Telegram API
+      }
+
+      // Handle incoming Telegram updates
+      await bot.handleUpdate(req.body); 
+
+      res.status(200).send('OK');
+    } catch (err) {
+      console.error('Error handling update:', err);
+      res.status(500).send('Internal Server Error');
+    }
   } else {
     res.status(405).end(); // Method Not Allowed
   }
