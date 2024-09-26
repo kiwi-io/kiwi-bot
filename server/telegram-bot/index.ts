91dba0ca01915b439559c8c59e2a560cbc6d520b
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-09-26 12:59:29 +0530

    Add an inline query support

--- server/telegram-bot/index.ts
+++ server/telegram-bot/index.ts
@@ -11,47 +11,47 @@ bot.on("message", (ctx) => {
 });
 
 
-// // Inline query handler for URLs
-// bot.on('inline_query', async (ctx) => {
-//   const queryText = ctx.inlineQuery.query;
+// Inline query handler for URLs
+bot.on('inline_query', async (ctx) => {
+  const queryText = ctx.inlineQuery.query;
 
-//   // Detect if the query contains a URL
-//   const urlRegex = /(https?:\/\/[^\s]+)/g;
-//   const urlMatch = queryText.match(urlRegex);
+  // Detect if the query contains a URL
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  const urlMatch = queryText.match(urlRegex);
 
-//   if (urlMatch) {
-//     const url = urlMatch[0];
+  if (urlMatch) {
+    const url = urlMatch[0];
 
-//     // Respond with a message that contains an image and buttons
-//     await ctx.answerInlineQuery([
-//       {
-//         type: 'article',
-//         id: '1',
-//         title: 'Share Kiwi App Link',
-//         description: 'Click to post a custom message with buttons',
-//         input_message_content: {
-//           message_text: `Check out this Kiwi link: ${url}`,
-//         },
-//         reply_markup: new InlineKeyboard()
-//           .url('Open Kiwi Mini App', `https://t.me/your_kiwi_bot?start=unique_text_1`)
-//           .row()
-//           .url('Another Button', `https://t.me/your_kiwi_bot?start=unique_text_2`),
-//       },
-//     ]);
-//   } else {
-//     // If no URL is found, return a default message
-//     await ctx.answerInlineQuery([
-//       {
-//         type: 'article',
-//         id: '2',
-//         title: 'No URL found',
-//         input_message_content: {
-//           message_text: 'Please tag a valid URL!',
-//         },
-//       },
-//     ]);
-//   }
-// });
+    // Respond with a message that contains an image and buttons
+    await ctx.answerInlineQuery([
+      {
+        type: 'article',
+        id: '1',
+        title: 'Share Kiwi App Link',
+        description: 'Click to post a custom message with buttons',
+        input_message_content: {
+          message_text: `Check out this Kiwi link: ${url}`,
+        },
+        reply_markup: new InlineKeyboard()
+          .url('Open Kiwi Mini App', `https://t.me/your_kiwi_bot?start=unique_text_1`)
+          .row()
+          .url('Another Button', `https://t.me/your_kiwi_bot?start=unique_text_2`),
+      },
+    ]);
+  } else {
+    // If no URL is found, return a default message
+    await ctx.answerInlineQuery([
+      {
+        type: 'article',
+        id: '2',
+        title: 'No URL found',
+        input_message_content: {
+          message_text: 'Please tag a valid URL!',
+        },
+      },
+    ]);
+  }
+});
 
 
 // bot.on('inline_query', async (ctx) => {
