4defea91aa02bf17a9f5c300c55f493b28c031e7
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-10-03 11:19:54 +0530

    fix 3

--- pages/send/index.tsx
+++ pages/send/index.tsx
@@ -95,7 +95,7 @@ const Send = () => {
   }
 
   const confirmSendHandler = async () => {
-    router.push(`/send-transaction-confirmation?from=${user.wallet.address}&to=${selectedRecipient}&token=${selectedTokenItem.address}&amount=${selectedAmount}`)
+    router.push(`/send-transaction-confirmation?from=${user.wallet.address}&to=${selectedRecipient}&token=${token}&amount=${selectedAmount}`)
   }
 
   return (
