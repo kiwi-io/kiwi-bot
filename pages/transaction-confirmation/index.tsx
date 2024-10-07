b372961f1ef7adb1269eb731cdced35eab77ef39
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-10-07 14:00:19 +0530

    Print sim result

--- pages/transaction-confirmation/index.tsx
+++ pages/transaction-confirmation/index.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./transaction-confirmation.module.css";
 import StandardHeader from "../../components/StandardHeader";
+import { simulateTransaction } from "../../utils";
 
 const TransactionConfirmation = () => {
+
+    useEffect(() => {
+        const doStuff = async() => {
+            await simulateTransaction();
+        }
+
+        doStuff();
+    }, []);
 
     return (
         <div className={styles.mainTransactionConfirmationContainer}>
--- utils/solana/index.ts
+++ utils/solana/index.ts
@@ -1,4 +1,6 @@
-import { ComputeBudgetProgram, TransactionInstruction } from "@solana/web3.js";
+import { ComputeBudgetProgram, Connection, PublicKey, TransactionInstruction } from "@solana/web3.js";
+import { NATIVE_SOL_PUBKEY } from "../../constants";
+import { getTransferTransaction, TransferParams } from "../token";
 
 export const requestComputeUnitsInstructions = (
     microLamportsPerCU: number,
@@ -17,3 +19,25 @@ export const requestComputeUnitsInstructions = (
         computeLimitIx
     ];
 }
+
+export const simulateTransaction = async () => {
+  const connection = new Connection(process.env.NEXT_RPC_MAINNET_URL);
+
+  const sender = new PublicKey("");
+  const recipient = new PublicKey("");
+  
+  const transferParams = {
+    connection,
+    fromPubkey: sender,
+    toPubkey: recipient,
+    token: NATIVE_SOL_PUBKEY,
+    tokenDecimals: 9,
+    amount: 100
+  } as TransferParams;
+
+  const transferTransaction = await getTransferTransaction(transferParams);
+  
+  const results = await connection.simulateTransaction(transferTransaction);
+
+  console.log("Results: ", results);
+} 
