5f2fa2586307407595f749c7fcda71e89ff16069
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-10-08 17:33:19 +0530

    fix logs

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -91,7 +91,7 @@ const Home = () => {
         console.log("starts with jup");
         const components = startParam.split("-");
 
-        const actionLink = components[1];
+        const actionLink = decodeTelegramCompatibleUrl(components[1]);
         console.log("components: ", components);
         console.log("passing: ", actionLink);
         router.push("/transaction-confirmation?actionUrl=", actionLink);        
