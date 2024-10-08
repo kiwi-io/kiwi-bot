d0235858ac65c1df5b0b73ea5ef74528811e7cec
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-10-08 11:03:40 +0530

    remove/comment all inline query answerers

--- server/telegram-bot/index.ts
+++ server/telegram-bot/index.ts
@@ -33,19 +33,6 @@ bot.on("message", async (ctx) => {
           keyboard.url(action.label, encodeTelegramCompatibleURL(actionApiUrl.origin + action.href)).row();
         }
        });
-  
-      // await ctx.answerInlineQuery([
-      //   {
-      //     type: "article",
-      //     id: "1",
-      //     title: "title",
-      //     description: "description",
-      //     input_message_content: {
-      //       message_text: "description",
-      //     },
-      //     // reply_markup: keyboard
-      //   },
-      // ]);
 
       await ctx.replyWithPhoto(getData.title, {
         caption: `<b>${getData.title}</b>\n\n${getData.description}`,
@@ -56,24 +43,6 @@ bot.on("message", async (ctx) => {
     }
     catch(err) {
       console.log("Error: ", err);
-
-      // await ctx.answerInlineQuery([
-      //   {
-      //     type: "article",
-      //     id: "1",
-      //     title: `Request a payment on Kiwi`,
-      //     description: `The received payment will be deposited on your Kiwi account`,
-      //     input_message_content: {
-      //       message_text: `Send SOL & memecoins using Kiwi`,
-      //     },
-      //     reply_markup: new InlineKeyboard()
-      //       .url(
-      //         "Pay using Kiwi",
-      //         `https://t.me/samplekiwibot/bot?startapp=send`,
-      //       )
-      //       .row()
-      //   },
-      // ]);
 
       await ctx.replyWithPhoto("title", {
         caption: `<b>${"Description"}</b>\n\n${"description"}`,
@@ -88,94 +57,58 @@ bot.on("message", async (ctx) => {
 });
 
 // Inline query handler for URLs
-bot.on("inline_query", async (ctx) => {
-  const queryText = ctx.inlineQuery.query;
+// bot.on("inline_query", async (ctx) => {
+//   const queryText = ctx.inlineQuery.query;
 
-  // Detect if the query contains a URL
-  const urlRegex = /(https?:\/\/[^\s]+)/g;
-  const urlMatch = queryText.match(urlRegex);
+//   // Detect if the query contains a URL
+//   const urlRegex = /(https?:\/\/[^\s]+)/g;
+//   const urlMatch = queryText.match(urlRegex);
 
-  if (urlMatch) {
-    const url = urlMatch[0];
+//   if (urlMatch) {
+//     const url = urlMatch[0];
 
-    const response: BeneficiaryParams = extractPaymentBeneficiaryFromUrl(url);
+//     const response: BeneficiaryParams = extractPaymentBeneficiaryFromUrl(url);
     
-    if(response && response.address) {
-      await ctx.answerInlineQuery([
-        {
-          type: "article",
-          id: "1",
-          title: `Request a payment on Kiwi`,
-          description: `The received payment will be deposited on ` + (response ? `${response.username}'s Kiwi wallet. Click 'space' to activate.` : ``),
-          input_message_content: {
-            message_text: response ? `🧾 ${response.username} is requesting a payment of ${response.amount} ${response.token}` : `You are requested to make a payment using Kiwi`,
-          },
-          reply_markup: new InlineKeyboard()
-            .url(
-              "Pay using Kiwi",
-              `https://t.me/samplekiwibot/bot?startapp=${encodeURIComponent(`send-${response.address}-${response.token}-${parseInt(response.amount.toString())}`)}`,
-            )
-            .row()
-        },
-      ]);
-    }
-    else {
-    await ctx.answerInlineQuery([
-      {
-        type: "article",
-        id: "1",
-        title: `Request a payment on Kiwi`,
-        description: `The received payment will be deposited on your Kiwi account`,
-        input_message_content: {
-          message_text: `Send SOL & memecoins using Kiwi`,
-        },
-        reply_markup: new InlineKeyboard()
-          .url(
-            "Pay using Kiwi",
-            `https://t.me/samplekiwibot/bot?startapp=send`,
-          )
-          .row()
-      },
-    ]);
-    }
-  }
-});
-
-// bot.on('inline_query', async (ctx) => {
-//     const queryText = ctx.inlineQuery.query;
-//     const urlRegex = /(https?:\/\/[^\s]+)/g;
-//     const urlMatch = queryText.match(urlRegex);
-
-//     if (urlMatch) {
-//       const url = urlMatch[0];
-
-//       // Respond with a custom image, message, and buttons
-      // await ctx.answerInlineQuery([
-      //   {
-      //     type: 'photo',
-      //     id: '1',
-      //     photo_url: 'https://raw.githubusercontent.com/Smaler1/coin/main/logo.png', // Replace with your image URL
-      //     thumbnail_url: 'https://raw.githubusercontent.com/Smaler1/coin/main/logo.png', // Thumbnail
-      //     caption: `Custom message for ${url}`,
-      //     reply_markup: new InlineKeyboard()
-      //       .url('Open Kiwi App', `https://t.me/your_kiwi_bot?start=/rewards?text=unique_text_1`)
-      //       .row()
-      //       .url('Another Button', `https://t.me/your_kiwi_bot?start=/rewards?text=unique_text_2`),
-      //   },
-      // ]);
-//     } else {
+//     if(response && response.address) {
 //       await ctx.answerInlineQuery([
 //         {
-//           type: 'article',
-//           id: '2',
-//           title: 'No URL found',
+//           type: "article",
+//           id: "1",
+//           title: `Request a payment on Kiwi`,
+//           description: `The received payment will be deposited on ` + (response ? `${response.username}'s Kiwi wallet. Click 'space' to activate.` : ``),
 //           input_message_content: {
-//             message_text: 'Please tag a valid URL!',
+//             message_text: response ? `🧾 ${response.username} is requesting a payment of ${response.amount} ${response.token}` : `You are requested to make a payment using Kiwi`,
 //           },
+//           reply_markup: new InlineKeyboard()
+//             .url(
+//               "Pay using Kiwi",
+//               `https://t.me/samplekiwibot/bot?startapp=${encodeURIComponent(`send-${response.address}-${response.token}-${parseInt(response.amount.toString())}`)}`,
+//             )
+//             .row()
 //         },
 //       ]);
 //     }
-//   });
+//     else {
+//     await ctx.answerInlineQuery([
+//       {
+//         type: "article",
+//         id: "1",
+//         title: `Request a payment on Kiwi`,
+//         description: `The received payment will be deposited on your Kiwi account`,
+//         input_message_content: {
+//           message_text: `Send SOL & memecoins using Kiwi`,
+//         },
+//         reply_markup: new InlineKeyboard()
+//           .url(
+//             "Pay using Kiwi",
+//             `https://t.me/samplekiwibot/bot?startapp=send`,
+//           )
+//           .row()
+//       },
+//     ]);
+//     }
+//   }
+// });
 
 // Launch the bot
 export const botWebhook = webhookCallback(bot, "next-js");
