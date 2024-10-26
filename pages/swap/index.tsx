08817e76b8872f3c22ca092e3dd46532b8ecfe82
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-10-26 15:58:13 +0530

    add swap button

--- pages/swap/index.tsx
+++ pages/swap/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./swap.module.css";
 import StandardHeader from "../../components/StandardHeader";
+import { Button } from "react-bootstrap";
+import { useTelegram } from "../../utils/twa";
+import { P } from "@privy-io/react-auth/dist/dts/types";
 
 const Swap = () => {
+
+  const [swapButtonText, setSwapButtonText] = useState<string>("Swap");
+  const { vibrate } = useTelegram();
+
   return (
     <div className={styles.swapPageContainer}>
       <StandardHeader title={`Trade`} backButtonNavigateTo={"home"} backButtonHide={true}/>
@@ -15,7 +22,15 @@ const Swap = () => {
             {
               [`7`, `8`, `9`, `4`, `5`, `6`, "1", `2`, `3`, `.`, `0`, <i className="fa-solid fa-arrow-left"></i>].map((value, index) => {
                 return (
-                  <div key={index} className={styles.numKeyContainer}>
+                  <div
+                    key={index}
+                    className={styles.numKeyContainer}
+                    onClick = {
+                      () => {
+                        vibrate("soft");
+                      }
+                    }
+                  >
                     {value}
                   </div>
                 )
@@ -23,7 +38,14 @@ const Swap = () => {
             }
           </div>
           <div className={styles.swapButtonContainer}>
-
+            <Button
+              className={styles.swapButton}
+              onClick = {
+                () => {
+                  vibrate("soft");
+                }
+              }
+            >{swapButtonText}</Button>
           </div>
         </div>
       </div>
