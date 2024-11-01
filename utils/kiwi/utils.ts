9cd96664fca48b3d08b926f668ba848ab7ad8eff
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-11-01 11:36:36 +0530

    correct api url

--- utils/kiwi/utils.ts
+++ utils/kiwi/utils.ts
@@ -13,7 +13,7 @@ export const getTelegramUserData = async (username: string) => {
 
 export const triggerNotification = async (userId: string, messageText: string) => {
   try {
-    const response = await axios.post("https://kiwi-bot.vercel.app/api/privy/trigger-notification", {
+    const response = await axios.post("https://kiwi-bot.vercel.app/api/trigger-notification", {
       userId,
       messageText,
     });
