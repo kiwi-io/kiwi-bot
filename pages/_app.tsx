caddf58d73311b36edfa32be48a6379ee589f22e
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-11-03 12:22:03 +0530

    only moonpay and manual for nowc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,7 +51,6 @@ function MyApp({ Component, pageProps }: AppProps) {
                   loginMethods: ["telegram"],
                   appearance: {
                     walletChainType: `solana-only`,
-                    walletList: ['phantom']
                   }
                 }}
               >
